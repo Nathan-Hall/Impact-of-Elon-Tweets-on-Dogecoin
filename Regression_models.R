0d474af4ec7cb44c4c7bb92772460351a848d532
Nathan Hall <50162797+Nathan-Hall@users.noreply.github.com>
2022-05-17 17:08:06 +1000

    Add files via upload

--- Regression_models.R
+++ Regression_models.R
@@ -5,12 +5,10 @@ library(lmtest)
 library(jtools) 
 library(car)
 
-setwd("~/UNI/ECMT3150/Group Assignment")
+#setwd("~/UNI/ECMT3150/Group Assignment")
 
 theme_set(theme_pubr())
 data <- read_excel("Elon_Dogecoin_Output.xlsx", sheet='Elon Tweets and CAR')
-
-car = log(1+data$CAR_5)
 
 ggplot(data, aes(Tweet_Count, CAR_60)) +
   geom_point() +
@@ -64,21 +62,7 @@ model120 <- lm(CAR_120 ~ Tweet_Count + Market_Cap + Picture + Video + Link + Rec
 summary(model120)
 
 
-anova(model, model_full)
-#There is strong evidence that the full model is not better
-
-plot(model)
-bptest(model)
-#Fail to reject null hypothesis - model is sufficiently homoskedastic
-
-summ(model, robust="HC1")
-
-dust(model) %>% 
-  sprinkle(cols = c("estimate", "std.error", "statistic"), round = 2) %>%
-  sprinkle(cols = "p.value", fn = quote(pvalString(value))) %>% 
-  sprinkle_colnames("Term", "Coefficient", "SE", "T-statistic", 
-                    "P-value")
-
+#WLS models
 wt <- 1 / lm(abs(model10$residuals) ~ model10$fitted.values)$fitted.values^2
 wls_model1 <- lm(CAR_10 ~ Tweet_Count + Market_Cap + Months + Tweet_Count*Market_Cap + Picture + Video + Link + Recognisability + hour_ret + hour_vol, data=data, weights=wt)
 summary(wls_model1)
